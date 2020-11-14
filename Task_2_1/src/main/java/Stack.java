3e58496cdfd5a2adc72c92f43dcf1eee1b8eac07
npanin1 <n.panin1@g.nsu.ru>
2020-11-14 20:15:44 +0700

    One more refactor of the Task_2_1

--- Task_2_1/src/main/java/Stack.java
+++ Task_2_1/src/main/java/Stack.java
@@ -1,52 +1,65 @@
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.*;
 
-public class Stack<Type> implements Iterable<Type> , Iterator<Type>{
-    private Type arr[];
+public class Stack<Type> implements Iterable<Type> {
+    private int modCount = 0;
+    private Type[] arr;
     private int cur;
     private int sz;
 
+    @Override
+    public Iterator<Type> iterator() {
+        Iterator<Type> it = new Iterator<Type>() {
+            private final int expectedModCount = modCount;
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
+                return currentIndex < cur && arr[currentIndex] != null;
+            }
+
+            @Override
+            public Type next() {
+
+                if (modCount != expectedModCount)
+                    throw new ConcurrentModificationException();
+                return arr[currentIndex++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
     @SuppressWarnings("unchecked")
     Stack() {
-        arr = (Type[])new Object[10];
+        arr = (Type[]) new Object[10];
         cur = 0;
         sz = 10;
     }
 
     public Type pop() {
-        if(cur == 0)
+        modCount++;
+        if (cur == 0)
             throw new NoSuchElementException("Stack is empty");
         return arr[--cur];
     }
 
-    public void push(Type inp){
-        if(cur == sz){
+    public void push(Type inp) {
+        modCount++;
+        if (cur == sz) {
             arr = Arrays.copyOf(arr, sz * 2);
             sz *= 2;
         }
         arr[cur++] = inp;
     }
 
-    public int count(){
+    public int count() {
         return cur;
     }
 
-    @Override
-    public Iterator<Type> iterator() {
-        return this;
-    }
-
-    @Override
-    public boolean hasNext() {
-        return cur > 0;
-    }
-
-    @Override
-    public Type next() {
-        if(!hasNext()){
-            throw new NoSuchElementException("Stack is empty");
-        }
-        return pop();
-    }
 }
