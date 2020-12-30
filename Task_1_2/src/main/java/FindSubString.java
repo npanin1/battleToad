73702bc94a4ae137ee4c58d009db07b047f824d2
https://github.com/npanin1/battleToad.git <n.panin1@g.nsu.ru>
2020-12-30 08:29:46 +0700

    Now the Task_1_2

--- Task_1_2/src/main/java/FindSubString.java
+++ Task_1_2/src/main/java/FindSubString.java
@@ -3,20 +3,21 @@ import java.io.IOException;
 
 public class FindSubString {
 
-    public static void SubString(String filepath, String sub) throws IOException {
+    public static int SubString(String filepath, String sub) throws IOException {
+        int res = -1;
         try(FileInputStream fin = new FileInputStream(filepath)){
-
             int i=-1, len = sub.length(), cnt = 0;;
             i=fin.read();
             while(i!=-1){
 
                 if((char)i == sub.charAt(0)){
                     int beg = cnt, local_cnt = cnt;
-                    while((i=fin.read())!=-1 && local_cnt - cnt != len && i == sub.charAt(local_cnt - beg + 1)){
+                    while((i=fin.read())!=-1 && local_cnt - cnt + 1 != len && i == sub.charAt(local_cnt - beg + 1)){
                         local_cnt++;
                     }
-                    if(local_cnt - cnt == len){
+                    if(local_cnt - cnt  + 1 == len){
                         System.out.println("{" + beg + "}");
+                        res = beg;
                         break;
                     }
                     else{
@@ -27,8 +28,11 @@ public class FindSubString {
                 cnt++;
                 i=fin.read();
             }
-            if(i == -1)
+            if(i == -1){
                 System.out.println("{}");
+
+            }
         }
+        return res;
     }
 }
--- Task_1_2/src/test/java/SubTest.java
+++ Task_1_2/src/test/java/SubTest.java
@@ -1,15 +1,25 @@
 import org.junit.Test;
 
+
 import java.io.IOException;
 
 public class SubTest {
     @Test
     public void test0(){
         try {
-            FindSubString.SubString("input.txt", "want");
+            int res = FindSubString.SubString("input.txt", "want");
+            assert res == 2;
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+    }
+    @Test
+    public void test1(){
+        try {
+            int res = FindSubString.SubString("input1.txt", "want");
+            assert res == 12;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
