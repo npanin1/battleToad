7619b08593e070ea16f8c30eae0515b3618ade14
https://github.com/npanin1/battleToad.git <n.panin1@g.nsu.ru>
2021-01-29 15:34:48 +0700

    refactor

--- Task_1_2/src/main/java/FindSubString.java
+++ Task_1_2/src/main/java/FindSubString.java
@@ -1,38 +1,65 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import static java.lang.Integer.max;
 
 public class FindSubString {
 
-    public static int SubString(String filepath, String sub) throws IOException {
-        int res = -1;
-        try(FileInputStream fin = new FileInputStream(filepath)){
-            int i=-1, len = sub.length(), cnt = 0;;
-            i=fin.read();
-            while(i!=-1){
+    public static ArrayList<Long> subString(InputStreamReader filepath, String sub) throws IOException {
 
-                if((char)i == sub.charAt(0)){
-                    int beg = cnt, local_cnt = cnt;
-                    while((i=fin.read())!=-1 && local_cnt - cnt + 1 != len && i == sub.charAt(local_cnt - beg + 1)){
-                        local_cnt++;
-                    }
-                    if(local_cnt - cnt  + 1 == len){
-                        System.out.println("{" + beg + "}");
-                        res = beg;
+        if (filepath == null) {
+            throw new NullPointerException("No such file");
+        }
+        if (sub == null) {
+            throw new NullPointerException("Substring to search is null");
+        }
+
+        ArrayList<Long> res = new ArrayList<>();
+
+        long cnt = 0;
+        int len = sub.length();
+        char[] buf = new char[len * 2];
+
+        int readed = filepath.read(buf, len, len);
+
+        if (readed < len) {
+            return res;
+        }
+        boolean lastScan = false;
+        do {
+            System.arraycopy(buf, len, buf, 0, len);
+            if (readed == len) {
+
+                readed = filepath.read(buf, len, len);
+                if(readed < len){
+                    readed = max(readed, 0);
+                    readed++;
+                    lastScan = true;
+                }
+            }
+
+            for (int i = 0; i < readed; i++) {
+                boolean isSubStr = false;
+                for (int j = 0; j < len; j++) {
+                    if (buf[i + j] != sub.charAt(j)) {
                         break;
                     }
-                    else{
-                        cnt = local_cnt;
-                        continue;
+                    if (j == len - 1) {
+                        isSubStr = true;
+                        break;
                     }
                 }
+                if (isSubStr) {
+                    res.add(cnt);
+                }
                 cnt++;
-                i=fin.read();
             }
-            if(i == -1){
-                System.out.println("{}");
+        } while (!lastScan);
 
-            }
-        }
+
         return res;
+
     }
 }
